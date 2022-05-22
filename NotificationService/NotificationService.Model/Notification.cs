2048f01d7d6087e67b9ded6e46333ebffee722e1
Nikola <nikolakabasaj6@gmail.com>
2022-05-22 10:09:28 +0200

    Project initialized

---
+++ NotificationService/NotificationService.Model/Notification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationService.Model
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Content { get; set; }
+
+        public string? Recipent { get; set; }
+
+        public NotificationType Type { get; set; }
+    }
+}
---
+++ NotificationService/NotificationService/Controllers/NotificationController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotificationService.Controllers
+{
+    [Route("api/[controller]")]
+    public class NotificationController : Controller
+    {
+    }
+}
