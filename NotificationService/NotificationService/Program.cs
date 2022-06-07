7533dbce7a6affa62964d93d85084ff02eb10168
Eva Janković <evaj.evadesign@gmail.com>
2022-06-07 12:28:43 +0200

    fix: Change ASP.NET version Program.cs

--- NotificationService/NotificationService/Program.cs
+++ NotificationService/NotificationService/Program.cs
@@ -1,26 +1,39 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using NotificationService.Service.Interface;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 
-namespace NotificationService
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+/*builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DislinktDbConnection")));
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);*/
+
+//repositories
+
+
+//services
+builder.Services.AddScoped<INotificationService, NotificationService.Service.NotificationService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
