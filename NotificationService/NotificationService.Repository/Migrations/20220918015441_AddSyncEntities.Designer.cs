1d2fd26f89dfdb71967ca6f9f36ed4615e9e5605
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-09-18 03:56:16 +0200

    feat: add sync entities migration

--- NotificationService/NotificationService.Repository/Migrations/20220918015441_AddSyncEntities.Designer.cs
+++ NotificationService/NotificationService.Repository/Migrations/20220918015441_AddSyncEntities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PostService.Repository;
@@ -11,9 +12,10 @@ using PostService.Repository;
 namespace NotificationService.Repository.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220918015441_AddSyncEntities")]
+    partial class AddSyncEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ NotificationService/NotificationService.Repository/Migrations/20220918015441_AddSyncEntities.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotificationService.Repository.Migrations
+{
+    public partial class AddSyncEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Connections",
+                schema: "notification",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Profile1 = table.Column<Guid>(type: "uuid", nullable: false),
+                    Profile2 = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connections", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                schema: "notification",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Surname = table.Column<string>(type: "text", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Connections",
+                schema: "notification");
+
+            migrationBuilder.DropTable(
+                name: "Profiles",
+                schema: "notification");
+        }
+    }
+}
